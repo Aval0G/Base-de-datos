353f1908d1eb66496251e8e3029a507547b19e7f
Aval0G <avaldez0@ucol.mx>
2023-09-08 16:19:20 -0600

    recursiva fibonacci

--- funciones-recursivas.js
+++ funciones-recursivas.js
@@ -1,6 +1,15 @@
-function f (x) {
+/* Funciones recursivas */
+
+function f (x) { // Función recursiva de factorial
   if (x <= 1) return 1;
   else return x * f(x - 1);
 }
 
 console.log(f(5));
+
+function fibonacci (n) { // Función recursiva de fibonacci
+  if (n <= 1) return 1;
+  else return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+console.log(fibonacci(5));
