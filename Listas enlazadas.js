e7ddc95718249946092491225c290f9748046f25
Aval0G <avaldez0@ucol.mx>
2023-10-04 11:30:14 -0600

    listas enlazadas

--- Listas enlazadas.js
+++ Listas enlazadas.js
@@ -17,9 +17,10 @@ class Estudiante {
 class Grupo {
   constructor () {
     this.primero = null;
+    this.ultimo = null;
   }
 
-  agregar (nuevo) {
+  /* agregar (nuevo) {
     if (this.primero == null) {
       this.primero = nuevo;
     } else {
@@ -28,6 +29,58 @@ class Grupo {
         aux = aux.siguiente;
       }
       aux.siguiente = nuevo;
+    }
+  } */
+
+  agregar (nuevo) {
+    if (this.primero == null) {
+      this.primero = nuevo;
+    } else {
+      this._agregar(nuevo, this.primero);
+    }
+  }
+
+  _agregar (nuevo, nodox) {
+    if (nodox.siguiente == null) {
+      nodox.siguiente = nuevo;
+    } else {
+      this._agregar(nuevo, nodox.siguiente);
+    }
+  }
+
+  agregarInicio (nuevo) {
+    if (this.primero == null) {
+      this.primero = nuevo;
+    } else {
+      nuevo.siguiente = this.primero;
+      this.primero = nuevo;
+    }
+  }
+
+  extraerInicio () {
+    const aux = this.primero;
+    this.primero = this.primero.siguiente;
+    aux.siguiente = null;
+    return aux;
+  }
+
+  extraerUltimo () {
+    if (this.primero == null) {
+      return null;
+    } else {
+      return this._extraerUltimo(this.primero);
+    }
+  }
+
+  _extraerUltimo (nodox) {
+    if (nodox.siguiente == null) {
+      return null;
+    } else if (nodox.siguiente.siguiente == null) {
+      const ultimoNodo = nodox.siguiente;
+      nodox.siguiente = null;
+      return ultimoNodo;
+    } else {
+      return this._extraerUltimo(nodox.siguiente);
     }
   }
 
@@ -42,11 +95,14 @@ class Grupo {
   }
 }
 
-let miGrupo = new Grupo();
+const miGrupo = new Grupo();
 let nuevo = new Estudiante('Ana', 1);
 miGrupo.agregar(nuevo);
 nuevo = new Estudiante('Bertha', 2);
 miGrupo.agregar(nuevo);
 nuevo = new Estudiante('Carlos', 3);
-miGrupo.agregar(nuevo);
+console.log(miGrupo.listar());
+miGrupo.agregarInicio(nuevo);
+console.log(miGrupo.extraerInicio());
+console.log(miGrupo.extraerUltimo());
 console.log(miGrupo.listar());
