2c140db9df8c4a5020d638b3f82fefdae60a1489
Aval0G <avaldez0@ucol.mx>
2023-10-06 15:13:14 -0600

    listas enlazadas final

--- aldo.js
+++ aldo.js
@@ -4,6 +4,7 @@ class Producto {
     this.nombre = nombre;
     this.precio = Number(precio);
     this.cantidad = Number(cantidad);
+    this.siguiente = null;
   }
 
   info () {
@@ -22,87 +23,110 @@ class Producto {
 
 class Inventario {
   constructor () {
-    this.productos = [];
+    this.primero = null;
+    this.ultimo = null;
   }
 
   buscar (codigo, factor) {
-    let menor = 0;
-    let mayor = this.productos.length - 1;
-    while (menor <= mayor) {
-      const puntoMedio = Math.floor(menor + (mayor - menor) / 2);
-      if (this.productos[puntoMedio].codigo == codigo) {
-        if (factor == 1) return this.productos[puntoMedio];
-        else if (factor == 2) return puntoMedio;
-      } else if (codigo < this.productos[puntoMedio].codigo) {
-        mayor = puntoMedio - 1;
-      } else {
-        menor = puntoMedio + 1;
+    let actual = this.primero;
+    let anterior = null;
+    while (actual != null) {
+      if (actual.codigo == codigo) {
+        if (factor == 1) return actual;
+        else return anterior;
       }
+      anterior = actual;
+      actual = actual.siguiente;
     }
-    if (factor == 1) return null;
-    else if (factor == 2) return -1;
   }
 
-  agregar (producto) {
-    if (this.productos.length === 0) {
-      this.productos.push(producto);
+  agregar (nuevo) {
+    if (this.primero == null) {
+      this.primero = nuevo;
+      this.ultimo = nuevo;
     } else {
-      let pos = 0;
-      while (pos < this.productos.length && producto.codigo >= this.productos[pos].codigo) {
-        pos++;
-      }
-
-      for (let i = this.productos.length; i > pos; i--) {
-        this.productos[i] = this.productos[i - 1];
-      }
-
-      this.productos[pos] = producto;
+      this.ultimo.siguiente = nuevo;
+      this.ultimo = nuevo;
     }
   }
 
   eliminar (codigo) {
-    const index = this.buscar(codigo, 2);
-    if (index != -1) {
-      for (let i = index; i < this.productos.length - 1; i++) {
-        this.productos[i] = this.productos[i + 1];
-      }
-      this.productos.pop();
+    let eliminar = this.buscar(codigo, 1);
+    const guardado = eliminar;
+    const anterior = this.buscar(codigo, 2);
+    anterior.siguiente = eliminar.siguiente;
+    eliminar = null;
+    return guardado;
+  }
+
+  modificar (nombre, precio, cantidad, codigo) {
+    const modificar = this.buscar(codigo, 1);
+    if (modificar != null) {
+      modificar.nombre = nombre;
+      modificar.precio = precio;
+      modificar.cantidad = cantidad;
       return true;
     } else {
       return false;
     }
   }
 
-  modificar (nombre, precio, cantidad, codigo) {
-    const indice = this.buscar(codigo, 2);
-    if (indice != -1) {
-      this.productos[indice].nombre = nombre;
-      this.productos[indice].precio = precio;
-      this.productos[indice].cantidad = cantidad;
+  insertarPosicion (producto, posicion) {
+    if (posicion == 1) {
+      producto.siguiente = this.primero;
+      this.primero = producto;
       return true;
-    } else {
-      return false;
+    }
+    let actual = this.primero;
+    let i = 1;
+    while (actual != null) {
+      if (posicion == i + 1) {
+        producto.siguiente = actual.siguiente;
+        actual.siguiente = producto;
+        return true;
+      }
+      i++;
+      actual = actual.siguiente;
     }
   }
 
   listar () {
     let html = '';
-    for (let i = 0; i < this.productos.length; i++) {
-      html += this.productos[i].infoHtml();
+    let actual = this.primero;
+    while (actual != null) {
+      html += actual.infoHtml();
+      actual = actual.siguiente;
     }
     return html;
   }
 
   listarInverso () {
-    let html = '';
-    for (let i = this.productos.length - 1; i >= 0; i--) {
-      html += this.productos[i].infoHtml();
+    return this._listarInverso(this.primero);
+  }
+
+  _listarInverso (nodo) {
+    if (nodo != null) {
+      const html = '' + this._listarInverso(nodo.siguiente);
+      return html + nodo.infoHtml();
+    } else {
+      return '';
     }
-    return html;
   }
 }
 
 const inventario = new Inventario();
+
+function precargar () {
+  inventario.agregar(new Producto(1, 'Papas', 10, 100));
+  inventario.agregar(new Producto(2, 'Manzanas', 20, 200));
+  inventario.agregar(new Producto(3, 'Peras', 30, 300));
+  inventario.agregar(new Producto(4, 'Naranjas', 40, 400));
+  inventario.agregar(new Producto(5, 'Sandias', 50, 500));
+  inventario.agregar(new Producto(6, 'Melones', 60, 600));
+  inventario.agregar(new Producto(7, 'Platanos', 70, 700));
+}
+
+precargar();
 
 const btnAdd = document.getElementById('btnAdd');
 btnAdd.addEventListener('click', () => {
@@ -132,8 +156,7 @@ btnClean.addEventListener('click', () => {
 const btnDelete = document.getElementById('btnDelete');
 btnDelete.addEventListener('click', () => {
   const codigo = document.getElementById('Codigo').value;
-  const eliminar = inventario.eliminar(codigo);
-  if (eliminar) document.getElementById('detalles').innerHTML = '<h2>Producto Elimnado</h2>';
+  if (inventario.eliminar(codigo) != null) document.getElementById('detalles').innerHTML = '<h2>Producto Eliminado</h2>';
   else { document.getElementById('detalles').innerHTML = '<h2>El producto no existe</h2>'; }
 });
 
@@ -154,7 +177,7 @@ btnModify.addEventListener('click', () => {
   const cantidad = document.getElementById('Cantidad').value;
   const codigo = document.getElementById('Codigo').value;
   const modificacion = inventario.modificar(nombre, precio, cantidad, codigo);
-  if (modificacion) document.getElementById('detalles').innerHTML = '<h2>Producto modificado</h2>' + inventario.buscar(codigo).infoHtml();
+  if (modificacion) document.getElementById('detalles').innerHTML = '<h2>Producto modificado</h2>' + inventario.buscar(codigo, 1).infoHtml();
   else { document.getElementById('detalles').innerHTML = '<h2>El producto no existe</h2>'; }
 });
 
@@ -164,4 +187,22 @@ btnSearch.addEventListener('click', () => {
   const producto = inventario.buscar(codigo, 1);
   if (producto != null) document.getElementById('detalles').innerHTML = producto.infoHtml();
   else { document.getElementById('detalles').innerHTML = '<h2>El producto no existe</h2>'; }
+});
+
+const btnInsert = document.getElementById('btnInsert');
+btnInsert.addEventListener('click', () => {
+  const nombre = document.getElementById('Nombre').value;
+  const precio = document.getElementById('Precio').value;
+  const cantidad = document.getElementById('Cantidad').value;
+  const codigo = document.getElementById('Codigo').value;
+  const posicion = document.getElementById('Posicion').value;
+  const producto = new Producto(codigo, nombre, precio, cantidad);
+  const comprobar = inventario.buscar(codigo, 1);
+  if (comprobar != null) {
+    document.getElementById('detalles').innerHTML = '<h2>El producto con ese codigo ya existe</h2>';
+  } else {
+    const insertar = inventario.insertarPosicion(producto, posicion);
+    if (insertar) document.getElementById('detalles').innerHTML = producto.infoHtml();
+    else document.getElementById('detalles').innerHTML = '<h2>Esa posicion no  existe</h2>';
+  }
 });
